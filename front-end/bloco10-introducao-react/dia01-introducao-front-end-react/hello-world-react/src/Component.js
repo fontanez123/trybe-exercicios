d51588bc7426592242b7955d3a6c897f8be4e929
Lázaro Kabib <lazarokabib94@gmail.com>
2022-01-26 20:13:50 -0300

    Bloco 10 - Começando React

---
+++ front-end/bloco10-introducao-react/dia01-introducao-front-end-react/hello-world-react/src/Component.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+class HelloWorld extends React.Component {
+    render(){
+        return <div>
+            <h1>Lázaro Kabib</h1>
+            <p>Começando com tudo em REACT!!</p>
+        </div>
+    }
+}
+
+export default HelloWorld;
